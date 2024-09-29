dac21fbb326f0083786453cbdada133dff5179bf
MOUPEPIDI NGUELET DERLY BORNIFELH <34122949+Bornifelh@users.noreply.github.com>
2024-09-29 20:57:06 +0100

    update header

--- src/pages/Header/Header.tsx
+++ src/pages/Header/Header.tsx
@@ -1,24 +1,31 @@
-import { IonButton, IonCol, IonHeader, IonIcon, IonNavLink, IonPage, IonRow, IonTitle, IonToolbar } from "@ionic/react";
+import { IonButton, IonCol, IonFab, IonFabButton, IonHeader, IonIcon, IonLabel, IonNavLink, IonPage, IonRow, IonTitle, IonToolbar } from "@ionic/react";
 import { cart, notifications } from "ionicons/icons";
 import React from "react";
 import Notifications from "../Notifications/Notifications";
 
 const Header : React.FC = () =>{
     return(
-        <IonHeader>
-      <IonToolbar>
-        <IonTitle>
-          <IonRow>
-              <IonCol size=''></IonCol>
-              <IonCol size='auto'>
-                <IonButton routerLink='./Notifications'>
-                  <IonIcon src={notifications}></IonIcon>
-                </IonButton>
-              </IonCol>
-          </IonRow>
-        </IonTitle>
-      </IonToolbar>
-    </IonHeader>
+    //     <IonHeader>
+    //   <IonToolbar>
+    //     <IonTitle>
+    //       <IonRow>
+    //           <IonCol size=''></IonCol>
+    //           <IonCol size='auto'>
+    //             <IonButton routerLink='./Notifications'>
+    //               <IonIcon src={notifications}></IonIcon>
+    //             </IonButton>
+    //           </IonCol>
+    //       </IonRow>
+    //     </IonTitle>
+    //   </IonToolbar>
+    // </IonHeader>
+
+    <IonFab  slot="fixed" vertical="top" horizontal="end">
+      <IonFabButton>
+      <IonIcon src={notifications}></IonIcon><IonLabel>2</IonLabel>
+      </IonFabButton>
+
+    </IonFab>
     )
 }
 export default Header;
