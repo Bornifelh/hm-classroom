9f3f11eb686b67bf6f9f2052e5f7fce09c49a870
MOUPEPIDI NGUELET DERLY BORNIFELH <34122949+Bornifelh@users.noreply.github.com>
2024-09-29 15:33:45 +0100

    add paiement

---
+++ src/pages/Paiement/Order.tsx
@@ -0,0 +1,14 @@
+import { IonContent, IonPage } from "@ionic/react";
+import React from "react";
+import "./Order.css";
+
+const Order : React.FC = () =>{
+    return(
+        <IonPage>
+            <IonContent fullscreen>
+
+            </IonContent>
+        </IonPage>
+    )
+}
+export default Order;
