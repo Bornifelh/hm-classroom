2c559f7122e155e6f9b292cc066acbfabf3185f3
MOUPEPIDI NGUELET DERLY BORNIFELH <34122949+Bornifelh@users.noreply.github.com>
2024-09-29 15:29:57 +0100

    add pub-content

---
+++ src/pages/Accueil/PubContent.tsx
@@ -0,0 +1,11 @@
+import { IonPage } from "@ionic/react";
+import React from "react";
+
+const PubContent : React.FC = () =>{
+    return(
+        <div>
+            
+        </div>
+    )
+}
+export default PubContent;
